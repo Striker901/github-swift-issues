46a5ef973f7d3f8eb56b755e07e1a96ea0d3c8bf
Vinícius D. Brandão <vinicius.brandao@mobile2you.com.br>
2021-07-26 10:58:50 -0300

    Initial Commit

---
+++ github-swift-issues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-swift-issues
+//
+//  Created by Vinícius D. Brandão on 26/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ github-swift-issuesTests/github_swift_issuesTests.swift
@@ -0,0 +1,33 @@
+//
+//  github_swift_issuesTests.swift
+//  github-swift-issuesTests
+//
+//  Created by Vinícius D. Brandão on 26/07/21.
+//
+
+import XCTest
+@testable import github_swift_issues
+
+class github_swift_issuesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
